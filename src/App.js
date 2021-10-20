055c1808f6bcdbf5b98d01cb53535a7e0650c05e
Zhirayr Harutyunyan <zhirayr.harutyunyan93@gmail.com>
2021-10-20 19:48:00 +0400

    Add project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.scss';
+import Header from './components/Header/Header';
+
+
+class App extends Component{
+  render() {
+    return (
+      <Header />
+    )
+  }
+}
+export default App;
