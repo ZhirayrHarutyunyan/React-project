d2402bc1cd58f1827ce4fbc6eb97ef833acfe1b3
Zhirayr <zhirayr.harutyunyan93@gmail.com>
2022-04-12 19:40:45 +0400

    Refactor

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React, { Component } from 'react';
-import './App.scss';
-import Header from './components/Header/Header';
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import { Product } from "./components/Product/Product";
 
-
-class App extends Component{
-  render() {
-    return (
+function App() {
+  return (
+    <>
       <Header />
-    )
-  }
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product" element={<Product />} />
+      </Routes>
+    </>
+  );
 }
 export default App;
